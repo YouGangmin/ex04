24b70169ed9c2fc56642be0052038207978ef6d0
YouGangmin <yot568@naver.com>
2023-04-08 19:40:43 +0900

    upgrade

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-const HomePage = () => {
-    return (
-        <Container>
-            <Row>
-                <Col className='text-center'>
-                    <h1>홈</h1>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-export default HomePage
+const HomePage = () => {
+  return (
+    <Container>
+      <Row>
+        <Col className='text-center'>
+          <h1>Welcome to my website</h1>
+          <p>
+            Feel free to explore!
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default HomePage;
--- src/components/TodosPage.jsx
+++ src/components/TodosPage.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
-import { Row, Col, Container } from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Row, Col, Container, Form, Button } from 'react-bootstrap'
 
 const TodosPage = () => {
-    return (
-        <Container>
-            <Row>
-                <Col className='text-center'>
-                    <h1>할일목록</h1>
-                </Col>
-            </Row>
-        </Container>
-    )
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+
+  const handleNewTodoChange = (event) => {
+    setNewTodo(event.target.value)
+  }
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault()
+    setTodos([...todos, newTodo])
+    setNewTodo('')
+  }
+
+  return (
+    <Container>
+      <Row>
+        <Col className='text-center'>
+          <h1>할일목록</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Form onSubmit={handleNewTodoSubmit}>
+            <Form.Group controlId='newTodo'>
+              <Form.Label>새 할일 추가</Form.Label>
+              <Form.Control type='text' value={newTodo} onChange={handleNewTodoChange} />
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              추가
+            </Button>
+          </Form>
+          <ul>
+            {todos.map((todo, index) => (
+              <li key={index}>{todo}</li>
+            ))}
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default TodosPage
